b692ee05eb4c0aa574b45bb60aa17c7723ce29b2
Al Sweigart <asweigart@gmail.com>
2018-08-27 00:35:07 -0700

    updated tests

--- tests/test_pyinputplus.py
+++ tests/test_pyinputplus.py
@@ -5,32 +5,18 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import sys
-import time
-import threading
 
-import pyautogui
-
+input = lambda: 'hello'
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pyinputplus as pyip
 
 
-class TypewriteThread(threading.Thread):
-    def __init__(self, msg, interval=0.0):
-        super(TypewriteThread, self).__init__()
-        self.msg = msg
-        self.interval = interval
-        self.start()
 
-
-    def run(self):
-        time.sleep(0.1) # Slight pause to give the other thread a chance to run the input*() function.
-        pyautogui.typewrite(self.msg, self.interval)
 
 
 def test_inputStr():
     # Test typical usage.
-    TypewriteThread('hello\n')
     assert pyip.inputStr() == 'hello'
 
 
